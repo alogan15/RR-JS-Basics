7fd2d497467cf9d4ade9f880f168c92c8dd6a467
Andre Logan <andrecommlink@aol.com>
2020-12-16 19:39:30 -0500

    java lab

--- js/formulas.js
+++ js/formulas.js
@@ -1,80 +1,80 @@
 // Basic math formulaas
 function addition(num1, num2){
-  return -1;
+  return num1 + num2;
 }
 
 function subtraction(num1, num2){
-  return -1;
+  return num1 - num2;
 }
 
 function multiplication(num1, num2){
-  return -1;
+  return num1 * num2;
 }
 
 function division(num1, num2){
-  return -1;
+  return num1 / num2;
 }
 
 // Area formulaas
 function areaSquare(side){
-  return -1;
+  return side ** 2;
 }
 
 function areaRectangle(length, width){
-  return -1;
+  return length * width;
 }
 
 function areaParallelogram(base, height){
-  return -1;
+  return base * height;
 }
 
 function areaTriangle(base, height){
-  return -1;
+  return .5 * base * height;
 }
 
 function Circle(radius){
-  return -1;
+  return 3.14 * radius**2;
 }
 
 function Sphere(radius){
-  return -1;
+  return 4 * 3.14 * radius **2;
 }
 
 // Surface Area formulas
 function surfaceAreaCube(side){
-  return -1;
+  return 6 * side **2;
 }
 
 function surfaceAreaCylinder(radius, height){
-  return -1;
+  return 2 * 3.14 * radius * height;
 }
 
 // Perimeter formulas
 function perimeterSquare(side){
-  return -1;
+  return 4 * square;
 }
 
 function perimeterRectangle(length, height){
-  return -1;
+  return 2 * (length + height);
 }
 
 function perimeterTriangle(side1, side2, side3){
-  return -1;
+  return side1 + side2 + side3;
 }
 
 function perimeterCircle(diameter){
-  return -1;
+  return 3.14 * diameter;
 }
 
 // Volume formulas
 function volumeCube(side){
-  return -1;
+  return side **3;
 }
 
 function volumeRectangular(length, width, height){
-  return -1;
+  return length * width * height;
 }
 
 function volumeCylinder(radius, height){
-  return -1;
+  return 3.14 * radius**2 * height **2;
 }
